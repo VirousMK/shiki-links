7f25d3b619941e2a5f22a98b30ca98121dccaa65
Virous <43663082+VirousMK@users.noreply.github.com>
2022-04-04 14:04:19 +0800

    5

--- js/js.js
+++ js/js.js
@@ -65,7 +65,7 @@ $.each(mangaList, function (i, title) {
 		}
 	});
 
-	if (i > 536 && flag) {
+	if (i > 654 && flag) {
 		sum++;
 		title.name = encodeURIComponent(title.name);
 		if (title.japanese) title.japanese = encodeURIComponent(title.japanese[0]);
