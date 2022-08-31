2154cc2ccf2902608234be127c214297cf20cf42
Virous <43663082+VirousMK@users.noreply.github.com>
2022-09-01 00:03:59 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -92,7 +92,7 @@ for (let i = 0; i < mangaList.length; i++) {
 		break;
 	}
 
-	if (i <= 501 || has_all_important_kinds) continue;
+	if (i <= 658 || has_all_important_kinds) continue;
 
 	sum++;
 	title.name = encodeURIComponent(title.name);
